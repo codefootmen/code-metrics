e7e30236ecbaee32069cfe54d1ec0d22bf427445
Flavio Barci <flavio.barci@gmail.com>
2019-11-17 16:36:46 -0300

    Fix var name error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const parseString = require("xml2js").parseString;
 const fs = require("fs");
 const cTable = require("console.table");
-var xml = fs.readFileSync(__dirname + "/model.xmi", "utf8");
+var xml = fs.readFileSync(__dirname + "/ClassDiagram.xmi", "utf8");
 
 let model = {};
 parseString(xml, function(err, result) {
   const classes =
-    result["xmi:XMI"]["uml:Model"][0]["packagedElement"][0]["packagedElement"];
+    result["xmi:XMI"]["uml:Model"][0].packagedElement[1].packagedElement[0]
+      .packagedElement[0].packagedElement;
   classes.forEach(x => {
     let nOfChildren = 0;
     classes.forEach(y => {
@@ -16,20 +17,21 @@ parseString(xml, function(err, result) {
         }
       }
     });
-    
-    let nOfParents =  "generalization" in x == true ? x["generalization"].length : 0;
+
+    let nOfParents =
+      "generalization" in x == true ? x["generalization"].length : 0;
     let noa = 0;
-    if(nOfParents > 0){
-      if(x["ownedAttribute"] != null){
+    if (nOfParents > 0) {
+      if (x["ownedAttribute"] != null) {
         x["ownedAttribute"].forEach(y => {
-          if(y["$"].isDerived === "false"){
+          if (y["$"].isDerived === "false") {
             noa++;
           }
         });
       }
-      if(x["ownedOperation"] != null){
-        x["ownedOparation"].forEach(y => {
-          if(y["$"].isDerived === "false"){
+      if (x["ownedOperation"] != null) {
+        x["ownedOperation"].forEach(y => {
+          if (y["$"].isDerived === "false") {
             noa++;
           }
         });
@@ -37,9 +39,11 @@ parseString(xml, function(err, result) {
     }
 
     model[x["$"].name] = {
-      ownedAttributes:"ownedAttribute" in x == true ? x["ownedAttribute"].length : 0,
+      ownedAttributes:
+        "ownedAttribute" in x == true ? x["ownedAttribute"].length : 0,
       ownedMembers: "ownedMember" in x == true ? x["ownedMember"].length : 0,
-      ownedOperations:"ownedOperation" in x == true ? x["ownedOperation"].length : 0,
+      ownedOperations:
+        "ownedOperation" in x == true ? x["ownedOperation"].length : 0,
       parents: nOfParents,
       children: nOfChildren,
       addBySubclass: noa
@@ -58,7 +62,7 @@ Object.keys(model).forEach(x => {
     CBO: model[x].ownedMembers,
     CS: model[x].ownedAttributes + model[x].ownedOperations,
     NOO: 0,
-    NOA: model[x].addBySubclass,
+    NOA: model[x].addBySubclass
   });
 });
 
